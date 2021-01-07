3fb547f04eec356116eda1d5852f42705c40c557
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-06 22:11:01 -0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+dotenv.config();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
