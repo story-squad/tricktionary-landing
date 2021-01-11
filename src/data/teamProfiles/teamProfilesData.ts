d022c074b4346994cea26ffc42a74ad1e0d4b754
Drew Ross <mail.drew.ross@gmail.com>
2021-01-11 11:55:33 -0800

    Add teamProfiles data with dummy profiles.

---
+++ src/data/teamProfiles/teamProfilesData.ts
@@ -0,0 +1,52 @@
+import { TeamProfileItem } from './teamProfilesTypes';
+
+export const teamProfiles: TeamProfileItem[] = [
+  {
+    name: 'Dragon Boi 1',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+  {
+    name: 'Dragon Boi 2',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+  {
+    name: 'Dragon Boi 3',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+  {
+    name: 'Dragon Boi 4',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+  {
+    name: 'Dragon Boi 5',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+  {
+    name: 'Dragon Boi 6',
+    role: 'Frontend Engineer',
+    photo:
+      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    bio:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero massa, dictum eu pharetra nec, ullamcorper ac elit. Ut id est tortor. In in hendrerit leo, eu sollicitudin orci.',
+  },
+];
