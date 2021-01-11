f92d85ebbcf8f9a888987b59985c18634f511f07
Drew Ross <mail.drew.ross@gmail.com>
2021-01-11 12:12:29 -0800

    Add profile object to props and fix renders.

--- src/components/pages/AboutPage/TeamProfiles/ProfileCard.tsx
+++ src/components/pages/AboutPage/TeamProfiles/ProfileCard.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { TeamProfileItem } from '../../../../data/teamProfiles/teamProfilesTypes';
 
-const ProfileCard = (props: TeamProfileItem): React.ReactElement => {
+const ProfileCard = (props: {
+  profile: TeamProfileItem;
+}): React.ReactElement => {
+  const { profile } = props;
   return (
     <div className="profile-card">
       <div className="profile-info">
-        <h3>{props.name}</h3>
-        <p>{props.role}</p>
-        <p>{props.bio}</p>
+        <h3>{profile.name}</h3>
+        <p>{profile.role}</p>
+        <p>{profile.bio}</p>
       </div>
       <div className="profile-img-wrapper">
-        <img src={props.photo} alt={`photo of ${props.name}`} />
+        <img src={profile.photo} alt={`photo of ${profile.name}`} />
       </div>
     </div>
   );
