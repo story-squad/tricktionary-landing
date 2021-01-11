3c6503f06f0122ae41bcf5becd6047f7dc78d3f2
Drew Ross <mail.drew.ross@gmail.com>
2021-01-11 11:20:26 -0800

    Update headers, add About paragraph with lorem ipsum.

--- src/components/pages/AboutPage/RenderAboutPage.tsx
+++ src/components/pages/AboutPage/RenderAboutPage.tsx
@@ -6,7 +6,16 @@ const RenderAboutPage = (): React.ReactElement => {
     <div className="about-page-wrapper">
       <Header />
       <div className="about-page">
-        <h2>About the Team</h2>
+        <h2>About Story Squad</h2>
+        <div className="content-wrapper">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            temporibus quia assumenda! At autem, similique rem, unde praesentium
+            quas aliquam quis quae reprehenderit deserunt, eum itaque aliquid.
+            Accusamus, natus officia.
+          </p>
+        </div>
+        <h2>Meet the Team</h2>
       </div>
     </div>
   );
