7bcf70b436d997889bdef18e2a5ac5ee4ec84fc4
Drew Ross <mail.drew.ross@gmail.com>
2021-01-11 11:35:43 -0800

    Set up TeamProfiles component outline.

---
+++ src/components/pages/AboutPage/TeamProfiles/ProfileCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileCard = (): React.ReactElement => {
+  return (
+    <div className="profile-card">
+      <p>Profile Card</p>
+    </div>
+  );
+};
+
+export default ProfileCard;
---
+++ src/components/pages/AboutPage/TeamProfiles/RenderProfiles.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RenderProfiles = (): React.ReactElement => {
+  return (
+    <div className="profile-container content-wrapper">
+      <p>Profiles</p>
+    </div>
+  );
+};
+
+export default RenderProfiles;
