77d53b962aec4161cb491633c0ee8e474ddc66a9
Drew Ross <mail.drew.ross@gmail.com>
2021-01-11 12:13:44 -0800

    Import teamProfiles data and map/render ProfileCards.

--- src/components/pages/AboutPage/TeamProfiles/RenderProfiles.tsx
+++ src/components/pages/AboutPage/TeamProfiles/RenderProfiles.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { teamProfiles } from '../../../../data';
+import ProfileCard from './ProfileCard';
 
 const RenderProfiles = (): React.ReactElement => {
   return (
     <div className="profile-container content-wrapper">
-      <p>Profiles</p>
+      {teamProfiles.map((profile) => (
+        <ProfileCard key={profile.name} profile={profile} />
+      ))}
     </div>
   );
 };
