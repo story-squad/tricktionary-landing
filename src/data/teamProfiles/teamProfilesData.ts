25a8f372227e012722413ccaffa516cc841a593c
Drew Ross <mail.drew.ross@gmail.com>
2021-01-12 12:44:59 -0800

    Import dragon-profile image for default profile pic. Update profile data.

--- src/data/teamProfiles/teamProfilesData.ts
+++ src/data/teamProfiles/teamProfilesData.ts
@@ -1,89 +1,79 @@
 import { TeamProfileItem } from './teamProfilesTypes';
+import dragonProfile from '../../assets/dragon-profile.png';
 
 export const teamProfiles: TeamProfileItem[] = [
   {
     name: 'Graig Peterson',
     role: 'Founder',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: `As a 6th Grade teacher, Graig watched the way  Fortnite and Tiktok were structuring his students' lives the moment the bell rang. He designed Story Squad to be an alternative to apps heavy on screen-time that aim to maximize a user's time-on-device; instead, Story Squad uses small servings of screen-time to maximize the time a kid spends in "imagination-mode" with a pencil in hand writing adventures and building worlds.`,
   },
   {
     name: 'Benjamin Griffin',
     role: 'Product Manager',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: 'Fullstack Web Developer, San Francisco',
   },
   {
     name: 'Brandon Ramirez',
     role: 'Frontend Lead',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio:
       'Full-Stack Web Developer in the Bay Area. Node, React, Deno, TypeScript.',
   },
   {
     name: 'AJ Gebara',
     role: 'Backend Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: '',
   },
   {
     name: 'Drew Ross',
     role: 'Frontend Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
-    bio: 'Coffee, climbing, and coding.',
+    photo: dragonProfile,
+    bio:
+      'Coffee, climbing, and coding. Fullstack Engineer in the Pacific Northwest.',
   },
   {
     name: 'Hailey Hansard',
     role: 'Frontend Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: '',
   },
   {
     name: 'Harper Atlas',
     role: 'UX/UI Desiger',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: '',
   },
   {
     name: 'Jesse Marek',
     role: 'Frontend Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: '',
   },
   {
     name: 'Jorge Manzur',
     role: 'Frontend Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: `I'm an individual that enjoys working collectively towards a goal. I embrace challenges and opportunities for personal growth. I see programming as an engineering tool that provides solutions that can improve many people's life, and I'm excited to be part of this promising industry.`,
   },
   {
     name: 'Lucas Greenwell',
     role: 'Growth Analyst',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: `I like technology, teaching, traveling, and granola!`,
   },
   {
     name: 'Trevor Martin',
     role: 'Full Stack Engineer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: ``,
   },
   {
     name: 'Victoria Gu',
     role: 'Marketer',
-    photo:
-      'https://fdsc-development.netlify.app/static/media/dragon-boi.d21c7981.svg',
+    photo: dragonProfile,
     bio: ``,
   },
 ];
