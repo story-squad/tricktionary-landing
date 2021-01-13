ed91276d2f366a6ed7a27d64c85c79e8548cfe57
Drew Ross <mail.drew.ross@gmail.com>
2021-01-13 12:36:41 -0800

    Update Graig's bio.

--- src/data/teamProfiles/teamProfilesData.ts
+++ src/data/teamProfiles/teamProfilesData.ts
@@ -14,7 +14,7 @@ export const teamProfiles: TeamProfileItem[] = [
     name: 'Graig Peterson',
     role: 'Founder',
     photo: dragonProfile,
-    bio: `As a 6th Grade teacher, Graig watched the way  Fortnite and Tiktok were structuring his students' lives the moment the bell rang. He designed Story Squad to be an alternative to apps heavy on screen-time that aim to maximize a user's time-on-device; instead, Story Squad uses small servings of screen-time to maximize the time a kid spends in "imagination-mode" with a pencil in hand writing adventures and building worlds.`,
+    bio: `Graig is a former 6th Grade teacher and PADI Divemaster who loves the Galapagos archipelago and Elizabeth Bishop’s poetry.`,
   },
   {
     name: 'Darwin M. Johnson',
