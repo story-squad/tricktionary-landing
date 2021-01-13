d79e11667020848f3d2c02a83a055f6c8e86f62f
Drew Ross <mail.drew.ross@gmail.com>
2021-01-13 12:37:11 -0800

    Update top text.

--- src/components/pages/AboutPage/RenderAboutPage.tsx
+++ src/components/pages/AboutPage/RenderAboutPage.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Header } from '../../common';
 import { TeamProfiles } from './TeamProfiles';
 
+const infoText = `As a 6th Grade teacher, Graig watched the way Fortnite and Tiktok
+were structuring his students' lives the moment the bell rang. He
+designed Story Squad to be an alternative to apps heavy on
+screen-time that aim to maximize a user's time-on-device; instead,
+Story Squad uses small servings of screen-time to maximize the time
+a kid spends in "imagination-mode" with a pencil in hand writing
+adventures and building worlds.`;
+
 const RenderAboutPage = (): React.ReactElement => {
   return (
     <div className="about-page-wrapper">
       <Header />
       <div className="about-page">
-        <h2>Our Mission:</h2>
+        <h2>Our Mission</h2>
         <div className="content-wrapper">
-          <p className="mission-statement">Get kids writing!</p>
+          <h3 className="mission-statement">Get kids writing!</h3>
+          <p className="info-text">{infoText}</p>
         </div>
         <h2>Meet the Team</h2>
         <TeamProfiles />
