770264da4048fb70081a071da5a1ae1e6dd065b1
Drew Ross <mail.drew.ross@gmail.com>
2021-01-15 10:34:46 -0800

    Remove team bios for consistency and styling.

--- src/data/teamProfiles/teamProfilesData.ts
+++ src/data/teamProfiles/teamProfilesData.ts
@@ -15,68 +15,55 @@ export const teamProfiles: TeamProfileItem[] = [
     name: 'Graig Peterson',
     role: 'Founder',
     photo: graigProfile,
-    bio: `Graig is a former 6th Grade teacher and PADI Divemaster who loves the Galapagos archipelago and Elizabeth Bishop’s poetry.`,
   },
   {
     name: 'Darwin M. Johnson',
     role: 'Founder',
     photo: darwinProfile,
-    bio: ``,
   },
   {
     name: 'Benjamin Griffin',
     role: 'Product Manager',
     photo: benProfile,
-    bio: 'Fullstack Web Developer, San Francisco',
   },
   {
     name: 'Brandon Ramirez',
     role: 'Frontend Lead',
     photo: brandonProfile,
-    bio:
-      'Full-Stack Web Developer in the Bay Area. Node, React, Deno, TypeScript.',
   },
   {
     name: 'Logan Van Hook',
     role: 'Backend Lead',
     photo: dragonProfile,
-    bio: `I'm passionate and love thinking outside the box to tackle obscure problems.`,
   },
   {
     name: 'Drew Ross',
     role: 'Frontend Engineer',
     photo: drewProfile,
-    bio:
-      'Coffee, climbing, and coding. Fullstack Engineer in the Pacific Northwest.',
   },
   {
     name: 'Jesse Marek',
     role: 'Frontend Engineer',
     photo: jesseProfile,
-    bio: '',
   },
   {
     name: 'Jorge Manzur',
     role: 'Frontend Engineer',
     photo: jorgeProfile,
-    bio: `I embrace challenges and opportunities for personal growth. I see programming as an engineering tool that provides solutions that can improve many people's lives, and I'm excited to be part of this promising industry.`,
   },
   {
     name: 'Lori Schlatter',
     role: 'Data Scientist',
     photo: loriProfile,
-    bio: `Hailing from Missouri, Lori is loving her foray into the world of data science after working for years in the nonprofit world. She loves anything to do with peanut butter and has read the Harry Potter series more times than she can count.`,
   },
   {
     name: 'Lucas Greenwell',
     role: 'Growth Analyst',
     photo: lucasProfile,
-    bio: `I like technology, teaching, traveling, and granola!`,
   },
   {
     name: 'Victoria Gu',
     role: 'Marketer',
     photo: dragonProfile,
-    bio: ``,
   },
 ];
