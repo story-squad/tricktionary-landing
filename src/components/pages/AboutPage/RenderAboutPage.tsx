0ad67dc23f0380f2f86fbbdec430132d19d84422
Lucas Greenwell <lucasgreenwell@Lucass-MBP.fios-router.home>
2021-03-03 14:39:27 -0500

    changes text on button and graigs photo

--- src/components/pages/AboutPage/RenderAboutPage.tsx
+++ src/components/pages/AboutPage/RenderAboutPage.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 import { Header } from '../../common';
 import { TeamProfiles } from './TeamProfiles';
 
-const infoText = `Story Squad is a team of tech nerds that want to help people express themselves creatively and connect with community. We've built a few different projects, below you can see our Tricktionary team. Tricktionary is a product designed to help start off video meetings by focusing everyone and giving participants a creative outlet. We have a strong commitment to never optimize for "time-in-app", which makes it hard to raise funds. If you'd like to help us keep building, you can support us on patreon.`;
+const infoText = `Our mission is to provide human connection through creative expression.
+Help us stay indie!`;
 
 const RenderAboutPage = (): React.ReactElement => {
   return (
     <div className="about-page-wrapper">
       <Header />
       <div className="about-page">
-        <h2>Our Mission</h2>
-        <div className="content-wrapper">
-          <h3 className="mission-statement">
+        {/* <h2>Our Mission</h2> */}
+        {/* <div className="content-wrapper"> */}
+        {/* <h3 className="mission-statement">
             Human connection through creative expression
-          </h3>
-          <p className="info-text">{infoText}</p>
-          <br />
-          <br />
-          <p>
-            <a href="http://patreon.com/storysquad">Support the team</a>
-          </p>
-        </div>
+          </h3> */}
+
+        {/* </div> */}
         <h2>Meet Our Team</h2>
+        <strong>
+          <p className="info-text">{infoText}</p>
+        </strong>
+        <br />
+        <br />
+        <p>
+          <a href="http://patreon.com/storysquad">support us on Patreon</a>
+        </p>
+        <br />
+        <br />
         <TeamProfiles />
       </div>
     </div>
